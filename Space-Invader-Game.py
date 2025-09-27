c08388cad9cf4f26ccca676083206e6bee2429ee
EvoGitH <95069962+EvoGitH@users.noreply.github.com>
2025-09-27 18:33:09 +0800

    Update Space-Invader-Game.py

--- Space-Invader-Game.py
+++ Space-Invader-Game.py
@@ -1,5 +1,4 @@
 import pygame
-import random
 
 # Initialize the pygame module
 pygame.init()
@@ -8,15 +7,17 @@ pygame.init()
 screenX = 800
 screenY = 600
 screen = pygame.display.set_mode((screenX, screenY))
+clock = pygame.time.Clock()
 
 # Title and Icon
 pygame.display.set_caption("Slime Invaders")
-icon = pygame.image.load('Game Assets\Slime.png')
+icon = pygame.image.load('Game Assets/Slime.png')
 pygame.display.set_icon(icon)
 
 # Player images
-playerimg = pygame.image.load('Game Assets\Slime.png')
-alienimg = pygame.image.load('Game Assets\Alien.png')
+playerimg = pygame.image.load('Game Assets/Slime.png')
+alienimg = pygame.image.load('Game Assets/Alien.png')
+
 # Scaling image size as it's over 64x64
 img_width = 64
 img_height = 64
@@ -26,33 +27,26 @@ img_scaled = pygame.transform.scale(playerimg, img_size)
 # Positioning of the player icon at first boot up
 playerX = 370
 playerY = 480
-# Positioning of the Alien icon, will be randomized at death or boot up.
-alienX = random.randint(0, 800)
-alienY = random.randint(50, 150) 
 
+# Positioning of the Alien icon at boot up.
+aliens_rect = alienimg.get_rect(topleft= (100, 50))
+ 
 # Allows for movement code to know how big your image/icon really is.
 iconX = 64
 iconY = 64
+
 # player's speed movement.
-player_speed = 0.1
-alien_speed = 0.3
-
-# Collision detection
-enemy_rect = pygame.Rect(alienX, alienY, screenX, screenY)
-wall_rect = pygame.Rect(screenX, screenY, 64, 64)
-
-# Self-Reminder, X is left & right, Y is up & down.
+player_speed = 2.5
+alien_speed = 3
 
 # Function is created using .blit() to draw player/alien images on top of window.
 def player(x, y):
     screen.blit(img_scaled, (x, y))
-def alien(x, y):
-    screen.blit(alienimg, (x, y))    
-
 
 # Game Loop to make sure the window is running/working with all game assets
 running = True
 while running:
+    dt = clock.tick(60) / 1000
     # RGB - Red, Green, Blue
     screen.fill((0, 150, 255)) #Creates the background colour
     for event in pygame.event.get():
@@ -72,22 +66,13 @@ while running:
         playerY += player_speed
     
     # Automated script for Alien movement
-    alienX.x =+ alien_speed
-    if alienX.right >= 600 or alienX.left <= 0:
-        alien_speed = -alien_speed #reverses direction
-        alien
+    aliens_rect.x += alien_speed * dt * 100
+    if aliens_rect.right >= screenX or aliens_rect.left <= 0:
+        alien_speed = -alien_speed
+        aliens_rect.y += 5
 
-
-
-
-# Creating Player boundaries so they don't go off screen.
     playerX = max(0, min(playerX, screenX - iconX))
     playerY = max(350, min(playerY, screenY - iconY))
-    alienX = max(0, min(alienX, screenX - iconX))
-    alienY = max(50, min(alienY, screenY - iconY))
-
-
-
+    screen.blit(alienimg, aliens_rect) 
     player(playerX, playerY)
-    alien(alienX, alienY)
     pygame.display.flip()
